4e321374d9222bba49b1480f7dd4e16a676c97ed
Bernardo Caputo <bernardocaputo@gmail.com>
2023-12-05 18:21:58 -0300

    feature: create links

---
+++ lib/scraper/links/link.ex
@@ -0,0 +1,22 @@
+defmodule Scraper.Links.Link do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Scraper.Pages.Page
+
+  schema "links" do
+    field :body, :string
+    field :url, :string
+    belongs_to :page, Page
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(link, attrs) do
+    link
+    |> cast(attrs, [:body, :url, :page_id])
+    |> validate_required([:body, :url, :page_id])
+    |> foreign_key_constraint(:page_id)
+  end
+end
